3e6bdf76021232c5b000b74db307f7025dd7e810
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-30 16:34:08 +0100

    add express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const host = 'http://127.0.0.1'
+const port = 3000
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on ${host}:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Benvenuto nel mio blog!</h1>');
+});
