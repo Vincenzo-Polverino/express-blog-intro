ad36cd876d62a281c419e4a8214bb14a9f0ee6dc
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-30 17:22:51 +0100

    add images

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio blog!</h1>');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
